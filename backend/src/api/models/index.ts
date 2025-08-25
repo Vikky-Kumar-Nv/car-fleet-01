c3019c081070c008170f8413a5711b1a7331a17e
vikky-coder-hub <vikkykumar700422@gmail.com>
2025-08-25 11:14:08 +0530

    fix driverReport.mode43

--- backend/src/api/models/index.ts
+++ backend/src/api/models/index.ts
@@ -7,7 +7,6 @@ export * from './booking.model';
 export * from './driverReport.model';
 export * from './payment.model';
 export * from './customer.model';
-export * from './driverReport.model';
 export * from './vehicleCategory.model';
 export * from './vehicleServicing.model';
 // export * from './city.model'; // will enable after file is present
